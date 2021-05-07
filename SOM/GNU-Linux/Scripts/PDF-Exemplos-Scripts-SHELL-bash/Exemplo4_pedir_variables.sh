fab3baf2c02136d1bf7872cfa478b57d49f70ba3
ricardofc <ricardofc.sistemas@gmail.com>
2021-05-07 22:08:33 +0200

    Update Exemplo4_pedir_variables.sh

--- SOM/GNU-Linux/Scripts/PDF-Exemplos-Scripts-SHELL-bash/Exemplo4_pedir_variables.sh
+++ SOM/GNU-Linux/Scripts/PDF-Exemplos-Scripts-SHELL-bash/Exemplo4_pedir_variables.sh
@@ -1,7 +1,7 @@
 # This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License
 # Creative Commons License: http://creativecommons.org/licenses/by-sa/4.0/
 # Author: Ricardo Feijoo Costa 
-# DATA: 2020-12-03 22:59:26.644228466 +0100
+# DATA: 2021-05-07 16:56:07.687172105 +0200
 
 #!/bin/bash             #Liña necesaria para saber que shell executará o script
 
@@ -9,3 +9,6 @@
 echo Dáme o teu nome    #Pedimos o nome do usuario
 read nome               #O que se introduce por teclado gárdase como variable nome
 echo Ola $nome          #Ensinamos por pantalla Ola e o contido da variable nome
+
+read -p 'Dáme o teu nome ' nome   #Pedimos o nome do usuario é o que se introduce por teclado gárdase como variable nome
+echo Ola $nome                   #Ensinamos por pantalla Ola e o contido da variable nome
