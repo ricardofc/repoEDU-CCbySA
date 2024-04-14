2727f3f01564808995fb5535eb9a69ed410a7241
ricardofc <ricardofc.sistemas@gmail.com>
2024-04-14 20:57:42 +0200

    Upload scripts env PDF-Comandos-e_SHELL_bash_3

---
+++ SOM/GNU-Linux/Scripts/PDF-Exemplos-Scripts-Comandos_e_SHELL_bash_3/env-exemplo3.sh
@@ -0,0 +1,10 @@
+# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License
+# Creative Commons License: http://creativecommons.org/licenses/by-sa/4.0/
+# Author: Ricardo Feijoo Costa 
+# DATA: 2024-04-14 20:55:15.717415352 +0100
+
+#!/bin/bash
+
+env | sort | grep -v '^_' | tee env1.txt
+cd /tmp
+declare -r LECTURA='read-only'
