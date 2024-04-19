2bfe426560f8733b6da6da2fb6477c794e811dbe
ricardofc <ricardofc.sistemas@gmail.com>
2024-04-19 17:33:45 +0200

    Exemplo10-v2 - Chamada a unha función antes de estar definida.

---
+++ SOM/GNU-Linux/Scripts/PDF-Exemplos-Scripts-SHELL-bash/Exemplo10-v2_funcions.sh
@@ -0,0 +1,16 @@
+# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License
+# Creative Commons License: http://creativecommons.org/licenses/by-sa/4.0/
+# Author: Ricardo Feijoo Costa 
+# DATA: 2020-12-03 23:18:52.603667802 +0100
+
+#!/bin/bash             #Liña necesaria para saber que shell executará o script
+
+suma                    #Chamamos á función suma antes da definición da función o que provocará un erro xa que o script execútase liña por lina de arriba a abaixo 
+                        ## e polo tanto non se pode chamar a unha función que aínda non está definida.
+suma() {                #Definimos a función suma
+echo Dame numero        #Pedimos un número
+read n1                 #Recollemos o número como valor na variable n1
+echo Dame outro numero  #Pedimos outro número
+read n2                 #Recollemos o número como valor na variable n2
+echo A suma de $n1 e $n2 é: $(($n1+$n2)) #Facemos a suma
+}                       #Finaliza a definición da función suma
